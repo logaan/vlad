723930695ebc380937f34f375d4084b574e667a1
Logan Campbell-McPherson <colin@logaan.net>
2012-06-15 00:32:05 +1000

    added description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject vlad "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Vlad is an attempt at providing convenient and simple
+               validations. Vlad is purely functional and makes no assumptions
+               about your data. It can be used for validating html form data as
+               just as well as it can be used to validate your csv about cats."
+  :url "https://github.com/logaan/vlad"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
