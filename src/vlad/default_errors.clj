0d0ec5921882a92b92f76de51773ba973b181d34
Logan Campbell-McPherson <colin@logaan.net>
2012-07-01 02:10:27 +1000

    wrote error default error messages for new validations

--- src/vlad/default_errors.clj
+++ src/vlad/default_errors.clj
@@ -1,6 +1,7 @@
 ;; Validations return a data structure that gives all information about an
 ;; error. You may find this information does not suit the tastes of your users.
-(ns vlad.default_errors)
+(ns vlad.default_errors
+  (:require [clojure.string :as s]))
 
 (defmulti translate
   "The translate function simply takes an error and returns a readable version
@@ -19,6 +20,26 @@
   [{:keys [name size]}]
   (format "%s must be under %s characters long." name size))
 
+(defmethod translate :vlad.validations/one-of
+  [{:keys [name set]}]
+  (format "%s must be one of %s." name (s/join ", " set)))
+
+(defmethod translate :vlad.validations/not-of
+  [{:keys [name set]}]
+  (format "%s must not be one of %s." name (s/join ", " set)))
+
+(defmethod translate :vlad.validations/equals-value
+  [{:keys [name value]}]
+  (format "%s must be \"%s\"." name value))
+
+(defmethod translate :vlad.validations/equals-field
+  [{:keys [first-name second-name]}]
+  (format "%s must be the same as %s." first-name second-name))
+
+(defmethod translate :vlad.validations/matches
+  [{:keys [name pattern]}]
+  (format "%s must match the pattern %s." name (.toString pattern)))
+
 (defn translate-errors
   "Translates a sequence of errors into a map of plain english error messages.
    Selectors are used as keys.
@@ -35,4 +56,3 @@
   (letfn [(translate-with-selector [error]
             {(:selector error) (translate error)})]
     (apply merge (map translate-with-selector errors))))
-
--- test/vlad/test/default_errors.clj
+++ test/vlad/test/default_errors.clj
@@ -12,6 +12,20 @@
   [{:type :vlad.validations/length-over :selector [:password] :name "Password" :size 8}]
   {[:password] "Password must be over 8 characters long."}
 
-  [{:type :vlad.validations/length-under :selector [:password] :name "Password" :size 8}]
-  {[:password] "Password must be under 8 characters long."})
+  [{:type :vlad.validations/one-of :selector [:title] :name "Title" :set #{"Mr" "Ms" "Mrs"}}]
+  {[:title] "Title must be one of Mr, Ms, Mrs."}
+
+  [{:type :vlad.validations/not-of :selector [:username] :name "Username" :set #{"login" "logout"}}]
+  {[:username] "Username must not be one of login, logout."}
+
+  [{:type :vlad.validations/equals-value :selector [:over_18] :name "Over 18" :value "yes"}]
+  {[:over_18] "Over 18 must be \"yes\"."}
+
+  [{:type :vlad.validations/equals-field
+    :first-name "Password" :first-selector [:password]
+    :second-name "Password confirmation" :second-selector [:password-confirmation]}]
+  {nil "Password must be the same as Password confirmation."}
+
+  [{:type :vlad.validations/matches :name "Username" :selector [:username] :pattern #"\w+"}]
+  {[:username] "Username must match the pattern \\w+."})
 
