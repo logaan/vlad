f04519938d1315fd734cba8e9d8da1d08705177a
Logan Campbell <colin@logaan.net>
2013-09-10 22:51:46 +1000

    removed name from validations

--- src/vlad/validations.clj
+++ src/vlad/validations.clj
@@ -7,71 +7,71 @@
   
   Example:
   
-    (validate (present \"Name\" :name)
+    (validate (present :name)
               {:name \"Vlad\"})"
-  [name selector]
+  [selector]
   (predicate selector str/blank?
-             {:type ::present :name name :selector selector}))
+             {:type ::present :selector selector}))
 
 (defn length-over 
   "Checks that the `count` of the value found at `selector` is over `size`."
-  [size name selector]
+  [size selector]
   (predicate selector #(> size (count %))
-             {:type ::length-over :size size :name name :selector selector}))
+             {:type ::length-over :size size :selector selector}))
 
 (defn length-under 
   "Checks that the `count` of the value found at `selector` is under `size`."
-  [size name selector]
+  [size selector]
   (predicate selector #(< size (count %))
-             {:type ::length-under :size size :name name :selector selector}))
+             {:type ::length-under :size size :selector selector}))
 
 (defn length-in 
   "Checks that the `count` of the value found at `selector` is over `lower` and
   under `upper`. No checking is done that `lower` is lower than `upper`. This
   validator may return multiple errors"
-  [lower upper name selector]
+  [lower upper selector]
   (join
-    (length-over  lower name selector)
-    (length-under upper name selector)))
+    (length-over  lower selector)
+    (length-under upper selector)))
 
 (defn one-of
   "Checks that the value found at `selector` is found within `set`"
-  [set name selector]
+  [set selector]
   (predicate selector #(not (contains? set %))
-             {:type ::one-of :set set :name name :selector selector}))
+             {:type ::one-of :set set :selector selector}))
 
 (defn not-of
   "Checks that the value found at `selector` is not found within `set`"
-  [set name selector]
+  [set selector]
   (predicate selector #(contains? set %)
-             {:type ::not-of :set set :name name :selector selector}))
+             {:type ::not-of :set set :selector selector}))
 
 (defn equals-value
   "Checks that the value found at `selector` is equal to the `value` that you
   provide."
-  [value name selector]
+  [value selector]
   (predicate selector #(not (= value %))
-             {:type ::equals-value :value value :name name :selector selector}))
+             {:type ::equals-value :value value :selector selector}))
 
 (defn equals-field
   "Checks that the values found at each of your selectors are equal to each
   other"
-  [first-name first-selector second-name second-selector]
+  [first-selector second-selector]
   (fn [data]
     (let [first-value  (get-in data first-selector)
           second-value (get-in data second-selector)]
       (if (= first-value second-value)
           []
           [{:type ::equals-field
-            :first-name first-name   :first-selector first-selector
-            :second-name second-name :second-selector second-selector}]))))
+            :first-selector first-selector
+            :second-selector second-selector}]))))
 
 (defn matches
   "Checks that the value found at `selector` is a regex match for `pattern`.
   This uses clojure's `re-matches` function which may not behave as you expect.
   Your pattern will have to match the whole string found at `selector` to count
   as a match."
-  [pattern name selector]
+  [pattern selector]
   (predicate selector #(nil? (re-matches pattern %))
-             {:type ::matches :pattern pattern :name name :selector selector}))
+             {:type ::matches :pattern pattern :selector selector}))
 
--- test/vlad/test/validations.clj
+++ test/vlad/test/validations.clj
@@ -6,87 +6,76 @@
   (fact (validate ?validator {:name "Chris" :confirm_name "Brad"}) => ?errors)
   ?validator ?errors
 
-  (present "Name" [:name])
+  (present [:name])
   []                   
 
-  (present "Age"  [:age])
+  (present [:age])
   [{:type :vlad.validations/present
-    :name "Age"
     :selector [:age]}] 
 
-  (length-over 4 "Name" [:name])
+  (length-over 4 [:name])
   []                   
 
-  (length-over 9 "Name" [:name])
+  (length-over 9 [:name])
   [{:type :vlad.validations/length-over
     :size 9
-    :name "Name"
     :selector [:name]}] 
 
-  (length-under 9 "Name" [:name])
+  (length-under 9 [:name])
   []                   
 
-  (length-under 4 "Name" [:name])
+  (length-under 4 [:name])
   [{:type :vlad.validations/length-under
     :size 4
-    :name "Name"
     :selector [:name]}] 
 
-  (length-in 4 9 "Name" [:name])
+  (length-in 4 9 [:name])
   []                   
 
-  (length-in 9 4 "Name" [:name])
+  (length-in 9 4 [:name])
   [{:type :vlad.validations/length-over
     :size 9
-    :name "Name"
     :selector [:name]}
    {:type :vlad.validations/length-under
     :size 4
-    :name "Name"
     :selector [:name]}]
 
-  (one-of #{"Chris" "Fred"} "Name" [:name])
+  (one-of #{"Chris" "Fred"} [:name])
   []
 
-  (one-of #{"Thelma" "Luise"} "Name" [:name])
+  (one-of #{"Thelma" "Luise"} [:name])
   [{:type :vlad.validations/one-of
     :set #{"Thelma" "Luise"}
-    :name "Name"
     :selector [:name]}]
 
-  (not-of #{"Thelma" "Luise"} "Name" [:name])
+  (not-of #{"Thelma" "Luise"} [:name])
   []
 
-  (not-of #{"Chris" "Fred"} "Name" [:name])
+  (not-of #{"Chris" "Fred"} [:name])
   [{:type :vlad.validations/not-of
     :set #{"Chris" "Fred"}
-    :name "Name"
     :selector [:name]}]
 
-  (equals-value "Chris" "Name" [:name])
+  (equals-value "Chris" [:name])
   []
 
-  (equals-value "Maddy" "Name" [:name])
+  (equals-value "Maddy" [:name])
   [{:type :vlad.validations/equals-value
     :value "Maddy"
-    :name "Name"
     :selector [:name]}] 
 
-  (equals-field "Name" [:name] "Name confirmation" [:name])
+  (equals-field [:name] [:name])
   []
 
-  (equals-field "Name" [:name] "Name confirmation" [:confirm_name])
+  (equals-field [:name] [:confirm_name])
   [{:type :vlad.validations/equals-field
-    :first-name "Name"
     :first-selector [:name]
-    :second-name "Name confirmation"
     :second-selector [:confirm_name]}]
   
-  (matches #"..ris" "Name" [:name])
+  (matches #"..ris" [:name])
   []
   
-  (matches #"andy" "Name" [:name])
+  (matches #"andy" [:name])
   (just [(contains {:type :vlad.validations/matches
-                              :name "Name"
                               :selector [:name]})]))
 
