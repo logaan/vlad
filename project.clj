811d41103dafda1fdb1e72a8d005c67b38eee326
Logan Campbell <colin@logaan.net>
2015-05-09 10:05:54 +1000

    dropped dev profile

--- project.clj
+++ project.clj
@@ -8,7 +8,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:doc {}
-             :dev {:source-paths  ["dev"]
-                   :plugins  [[lein-midje "3.1.1"]]
+             :dev {:plugins  [[lein-midje "3.1.1"]]
                    :dependencies [[midje "1.5.1"]]}})
-
