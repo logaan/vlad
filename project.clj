097869e7fa45a289562291e69f31f86b249b54e8
Logan Campbell <colin@logaan.net>
2015-05-13 11:20:44 +1000

    validations without midje

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vlad "2.0.0"
+(defproject vlad "2.1.0"
   :description "Vlad is an attempt at providing convenient and simple
                validations. Vlad is purely functional and makes no assumptions
                about your data. It can be used for validating html form data
@@ -7,7 +7,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta1"]]
-  :profiles {:doc {}
-             :dev {:plugins  [[lein-midje "3.1.3"]]
-                   :dependencies [[midje "1.7.0-beta1"]]}}
+  :profiles {:doc {}}
   :test-matcher #"vlad\.test\..*")
--- test/vlad/test/validations.clj
+++ test/vlad/test/validations.clj
@@ -1,85 +1,89 @@
 (ns vlad.test.validations
   (:require [vlad.core :refer :all]
-            [midje.sweet :refer [tabular fact just contains]]))
+            [clojure.test :refer :all]))
 
-(tabular
-  (fact (validate ?validator {:name "Chris" :confirm_name "Brad" :number-of-teeth 32}) => ?errors)
-  ?validator ?errors
+(def data
+  {:name "Chris" :confirm_name "Brad" :number-of-teeth 32})
 
-  (attr [:name] present)
-  []                   
+(deftest validations
+  (are
+    [?validator ?errors]
+    (= ?errors (validate ?validator data))
 
-  (attr [:number-of-teeth] present)
-  [{:type :vlad.core/present
-    :selector [:number-of-teeth]}] 
+    (attr [:name] present)
+    []                   
 
-  (attr [:age] present)
-  [{:type :vlad.core/present
-    :selector [:age]}] 
+    (attr [:number-of-teeth] present)
+    [{:type :vlad.core/present
+      :selector [:number-of-teeth]}] 
 
-  (attr [:name] (length-over 4))
-  []                   
+    (attr [:age] present)
+    [{:type :vlad.core/present
+      :selector [:age]}] 
 
-  (attr [:name] (length-over 9))
-  [{:type :vlad.core/length-over
-    :size 9
-    :selector [:name]}] 
+    (attr [:name] (length-over 4))
+    []                   
 
-  (attr [:name] (length-under 9))
-  []                   
+    (attr [:name] (length-over 9))
+    [{:type :vlad.core/length-over
+      :size 9
+      :selector [:name]}] 
 
-  (attr [:name] (length-under 4))
-  [{:type :vlad.core/length-under
-    :size 4
-    :selector [:name]}] 
+    (attr [:name] (length-under 9))
+    []                   
 
-  (attr [:name] (length-in 4 9))
-  []                   
+    (attr [:name] (length-under 4))
+    [{:type :vlad.core/length-under
+      :size 4
+      :selector [:name]}] 
 
-  (attr [:name] (length-in 9 4))
-  [{:type :vlad.core/length-over
-    :size 9
-    :selector [:name]}
-   {:type :vlad.core/length-under
-    :size 4
-    :selector [:name]}]
+    (attr [:name] (length-in 4 9))
+    []                   
 
-  (attr [:name] (one-of #{"Chris" "Fred"}))
-  []
+    (attr [:name] (length-in 9 4))
+    [{:type :vlad.core/length-over
+      :size 9
+      :selector [:name]}
+     {:type :vlad.core/length-under
+      :size 4
+      :selector [:name]}]
 
-  (attr [:name] (one-of #{"Thelma" "Luise"}))
-  [{:type :vlad.core/one-of
-    :set #{"Thelma" "Luise"}
-    :selector [:name]}]
+    (attr [:name] (one-of #{"Chris" "Fred"}))
+    []
 
-  (attr [:name] (not-of #{"Thelma" "Luise"}))
-  []
+    (attr [:name] (one-of #{"Thelma" "Luise"}))
+    [{:type :vlad.core/one-of
+      :set #{"Thelma" "Luise"}
+      :selector [:name]}]
 
-  (attr [:name] (not-of #{"Chris" "Fred"}))
-  [{:type :vlad.core/not-of
-    :set #{"Chris" "Fred"}
-    :selector [:name]}]
+    (attr [:name] (not-of #{"Thelma" "Luise"}))
+    []
 
-  (attr [:name] (equals-value "Chris"))
-  []
+    (attr [:name] (not-of #{"Chris" "Fred"}))
+    [{:type :vlad.core/not-of
+      :set #{"Chris" "Fred"}
+      :selector [:name]}]
 
-  (attr [:name] (equals-value "Maddy"))
-  [{:type :vlad.core/equals-value
-    :value "Maddy"
-    :selector [:name]}] 
+    (attr [:name] (equals-value "Chris"))
+    []
 
-  (equals-field [:name] [:name])
-  []
+    (attr [:name] (equals-value "Maddy"))
+    [{:type :vlad.core/equals-value
+      :value "Maddy"
+      :selector [:name]}] 
 
-  (equals-field [:name] [:confirm_name])
-  [{:type :vlad.core/equals-field
-    :first-selector [:name]
-    :second-selector [:confirm_name]}]
-  
-  (attr [:name] (matches #"..ris"))
-  []
-  
-  (attr [:name] (matches #"andy"))
-  (just [(contains {:type :vlad.core/matches
-                              :selector [:name]})]))
+    (equals-field [:name] [:name])
+    []
+
+    (equals-field [:name] [:confirm_name])
+    [{:type :vlad.core/equals-field
+      :first-selector [:name]
+      :second-selector [:confirm_name]}]
+
+    (attr [:name] (matches #"..ris"))
+    [])
+
+  (let [error (first (validate (attr [:name] (matches #"andy")) data))]
+    (is (= :vlad.core/matches (:type error)))
+    (is (= [:name] (:selector error)))))
 
